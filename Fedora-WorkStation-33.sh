0d46ac1cb19db341777daf3f964cc0d3ffd92ee9
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-04-06 11:49:06 -0400

    Update Fedora-WorkStation-33.sh

--- Fedora-WorkStation-33.sh
+++ Fedora-WorkStation-33.sh
@@ -113,6 +113,7 @@ find /home/${USER}/Mojave-CT -name '*[Tt]hunderbird*' -exec rm {} \;
 find /home/${USER}/Mojave-CT -name '*[Mm]inecraft*' -exec rm {} \;
 find /home/${USER}/Mojave-CT -name '*[Ee]piphany*' -exec rm {} \;
 find /home/${USER}/Mojave-CT -name '*[Rr]iot*' -exec rm {} \;
+find /home/${USER}/Mojave-CT -name '*[Ss]tartup*' -exec rm {} \;
 gsettings set org.gnome.desktop.interface icon-theme "Arc"
 
 #Set GTK theme
