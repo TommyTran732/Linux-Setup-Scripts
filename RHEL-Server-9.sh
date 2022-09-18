eb421d1fe451f3041f6f8a743b29ee65e55aa709
Tommy <contact@tommytran.io>
2022-09-17 20:41:18 -0400

    SSHD Configurations
    Signed-off-by: Tommy <contact@tommytran.io>

--- RHEL-Server-9.sh
+++ RHEL-Server-9.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #Assuming that you are using ANSSI-BP-028
 
+mkdir -p /etc/ssh/ssh_config.d /etc/ssh/sshd_config.d 
+echo "GSSAPIAuthentication no" | sudo tee -a /etc/ssh/ssh_config.d/10-custom.conf
+echo "X11Forwarding no 
+GSSAPIAuthentication no" | sudo tee -a /etc/ssh/sshd_config.d/10-custom.conf
+echo "PasswordAuthentication no" | sudo tee /etc/ssh/sshd_config.d/40-disable-passwords.conf
+
 sudo dnf install tuned -y
 sudo tuned-adm profile virtual-guest
-
-echo "X11Forwarding no" >> sudo tee -a /etc/ssh/sshd_config.d/10-custom.conf
-echo "GSSAPIAuthentication no" >> sudo tee -a /etc/ssh/sshd_config.d/10-custom.conf
-echo "GSSAPIAuthentication no" >> sudo tee -a /etc/ssh/ssh_config.d/10-custom.conf
 
 sudo curl https://raw.githubusercontent.com/Whonix/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf -o /etc/sysctl.d/30_security-misc.conf
