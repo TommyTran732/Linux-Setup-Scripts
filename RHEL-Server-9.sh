6b1ad67453b859cf9ffee56d08b272bb5804d022
Tommy <contact@tommytran.io>
2022-11-18 22:06:30 -0500

    Change link to Kicksecure
    Signed-off-by: Tommy <contact@tommytran.io>

--- RHEL-Server-9.sh
+++ RHEL-Server-9.sh
@@ -10,7 +10,7 @@ echo "PasswordAuthentication no" | sudo tee /etc/ssh/sshd_config.d/40-disable-pa
 sudo dnf install tuned -y
 sudo tuned-adm profile virtual-guest
 
-sudo curl https://raw.githubusercontent.com/Whonix/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
+sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf -o /etc/sysctl.d/30_security-misc.conf
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_silent-kernel-printk.conf -o /etc/sysctl.d/30_silent-kernel-printk.conf
 sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony.conf
