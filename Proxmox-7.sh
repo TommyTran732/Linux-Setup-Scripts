2ac5ffc9da54812c2d9e21189bf7a40d07c92d91
Tommy <contact@tommytran.io>
2022-11-20 16:48:03 -0500

    Update Proxmox-7.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- Proxmox-7.sh
+++ Proxmox-7.sh
@@ -6,7 +6,7 @@ apt update
 apt upgrade -y
 apt install -y intel-microcode tuned apparmor-profiles fwupd
 
-tuned-adm profile virt-host
+tuned-adm profile virtual-host
 
 ### This part assumes that you are using systemd-boot
 echo -e "spectre_v2=on spec_store_bypass_disable=on l1tf=full,force mds=full,nosmt tsx=off tsx_async_abort=full,nosmt kvm.nx_huge_pages=force nosmt=force l1d_flush=on mmio_stale_data=full,nosmt random.trust_bootloader=off random.trust_cpu=off intel_iommu=on amd_iommu=on efi=disable_early_pci_dma iommu.passthrough=0 iommu.strict=1 slab_nomerge init_on_alloc=1 init_on_free=1 pti=on vsyscall=none page_alloc.shuffle=1 randomize_kstack_offset=on extra_latent_entropy debugfs=off $(cat /etc/kernel/cmdline)" > /etc/kernel/cmdline
