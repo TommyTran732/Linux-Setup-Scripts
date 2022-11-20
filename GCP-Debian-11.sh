c2b856b24e12d1f554befc85474b4c477165cac4
Tommy <contact@tommytran.io>
2022-11-20 16:47:54 -0500

    Create GCP-Debian-11.sh
    Signed-off-by: Tommy <contact@tommytran.io>

---
+++ GCP-Debian-11.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+apt install -y tuned apparmor-profiles ufw
+
+tuned-adm profile virtual-guest
+
+curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
+curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf -o /etc/sysctl.d/30_security-misc.conf
+curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_silent-kernel-printk.conf -o /etc/sysctl.d/30_silent-kernel-printk.conf
+sed -i 's/kernel.yama.ptrace_scope=2/kernel.yama.ptrace_scope=3/g' /etc/sysctl.d/30_security-misc.conf
+
+rm -rf /etc/chrony/chrony.conf
+curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony/chrony.conf
+
+echo "* hard core 0" | tee -a /etc/security/limits.conf
+
+sed -i 's/#GSSAPIAuthentication no/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
