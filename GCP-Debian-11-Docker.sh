8e7fffb93d132d720a6168245dd19202b33ee8f5
Tommy <contact@tommytran.io>
2023-03-17 12:51:38 -0400

    Add --no-install-recommends
    Signed-off-by: Tommy <contact@tommytran.io>

--- GCP-Debian-11-Docker.sh
+++ GCP-Debian-11-Docker.sh
@@ -7,12 +7,12 @@ sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt -y install docker-ce
+sudo apt -y --no-install-recommends install docker-ce
 
 # Install gVisor
 curl -fsSL https://gvisor.dev/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/gvisor-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/gvisor-archive-keyring.gpg] https://storage.googleapis.com/gvisor/releases release main" | sudo tee /etc/apt/sources.list.d/gvisor.list > /dev/null
 sudo apt update 
-sudo apt -y install runsc
+sudo apt -y --no-install-recommends install runsc
 sudo runsc install
 sudo systemctl restart docker
