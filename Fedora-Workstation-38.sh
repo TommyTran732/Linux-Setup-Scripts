c88da56eb242d5075150ed4fd5dc2b35690c3579
Tommy <contact@tommytran.io>
2023-09-28 16:33:41 -0700

    Update Fedora-Workstation-38.sh
    Signed-off-by: Tommy <contact@tommytran.io>

--- Fedora-Workstation-38.sh
+++ Fedora-Workstation-38.sh
@@ -123,7 +123,7 @@ sudo dnf -y remove fedora-bookmarks fedora-chromium-config firefox mozilla-files
     #Remove Gnome apps
     gnome-photos gnome-connections gnome-tour gnome-themes-extra gnome-screenshot gnome-remote-desktop gnome-font-viewer gnome-calculator gnome-calendar gnome-contacts \
     gnome-maps gnome-weather gnome-logs gnome-boxes gnome-disk-utility gnome-clocks gnome-color-manager gnome-characters baobab totem \
-    gnome-shell-extension-background-logo gnome-shell-extension-apps-menu gnome-shell-extension-horizontal-workspaces gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list \
+    gnome-shell-extension-background-logo gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list \
     gnome-classic* gnome-user* gnome-text-editor chrome-gnome-shell eog \
     #Remove apps
     rhythmbox yelp evince libreoffice* cheese file-roller* mediawriter \
