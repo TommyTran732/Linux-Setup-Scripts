b2d89126fba5ad24bbe8647093e1083843316b59
Tommy <contact@tommytran.io>
2024-01-02 07:19:41 -0700

    Fix Edge setup
    Signed-off-by: Tommy <contact@tommytran.io>

--- Fedora-Workstation-39.sh
+++ Fedora-Workstation-39.sh
@@ -136,10 +136,10 @@ if [ "${MACHINE_TYPE}" == 'x86_64' ]; then
     sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
     sudo dnf install -y microsoft-edge-stable
     sudo mkdir -p /etc/opt/edge/policies/managed/ /etc/opt/edge/policies/recommended/
-    sudo chmod 755 /etc/opt/edge/policies/managed/ /etc/opt/edge/policies/recommended/
+    sudo chmod -R 755 /etc/opt/edge
     unpriv curl https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/managed.json | sudo tee /etc/opt/edge/policies/managed/managed.json
     sudo chmod 644 /etc/opt/edge/policies/managed/managed.json
-    unpriv curl https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/recommended.json | sudo tee /etc/opt/edge/policies/managed/recommended.json
+    unpriv curl https://raw.githubusercontent.com/TommyTran732/Microsoft-Edge-Policies/main/Linux/recommended.json | sudo tee /etc/opt/edge/policies/recommended/recommended.json
     sudo chmod 644 /etc/opt/edge/policies/managed/managed.json /etc/opt/edge/policies/managed/recommended.json
     sudo chmod 644 /etc/opt/edge/policies/managed/recommended.json
 fi
