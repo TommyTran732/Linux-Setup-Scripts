a187c7fc46273bdee9237876283ee365911f5c28
Tommy <contact@tommytran.io>
2024-02-03 06:18:58 -0700

    Typo fix
    Signed-off-by: Tommy <contact@tommytran.io>

--- Proxmox-8.sh
+++ Proxmox-8.sh
@@ -50,7 +50,7 @@ sed -i '1 {s/^/#/}' /etc/apt/sources.list.d/pve-enterprise.list
 
 echo 'deb https://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
 
-deb http://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian-security/ bookworm-security main contrib non-free non-free-firmware
 
 deb https://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
 
