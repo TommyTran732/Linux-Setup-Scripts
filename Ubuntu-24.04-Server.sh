3336de3f2378b545cd194d5ddb1d1257a7935319
Tommy <contact@tommytran.io>
2024-10-15 14:16:16 -0700

    Fix systemd override config

--- Ubuntu-24.04-Server.sh
+++ Ubuntu-24.04-Server.sh
@@ -57,7 +57,7 @@ unpriv curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Script
 sudo chmod 644 /etc/ssh/ssh_config.d/10-custom.conf
 sudo mkdir -p /etc/systemd/system/ssh.service.d/
 sudo chmod 755 /etc/systemd/system/ssh.service.d/
-unpriv curl -s https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/systemd/system/sshd.service.d/local.conf | sudo tee /etc/systemd/system/ssh.service.d/override.conf > /dev/null
+unpriv curl -s https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/systemd/system/sshd.service.d/override.conf | sudo tee /etc/systemd/system/ssh.service.d/override.conf > /dev/null
 sudo chmod 644 /etc/systemd/system/ssh.service.d/override.conf
 sudo systemctl daemon-reload
 sudo systemctl restart ssh
