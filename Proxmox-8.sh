fd48978c357c4b53221303cbdcded5bb79603f5d
Tommy <contact@tommytran.io>
2025-05-25 03:57:12 -0400

    Add ZRAM config to Proxmox
    Signed-off-by: Tommy <contact@tommytran.io>

--- Proxmox-8.sh
+++ Proxmox-8.sh
@@ -63,7 +63,7 @@ apt full-upgrade -y
 apt autoremove -y
 
 # Install packages
-apt install -y intel-microcode proxmox-kernel-6.14 tuned
+apt install -y intel-microcode proxmox-kernel-6.14 systemd-zram-generator tuned
 
 ### This part assumes that you are using systemd-boot
 echo "mitigations=auto,nosmt spectre_v2=on spectre_bhi=on spec_store_bypass_disable=on tsx=off kvm.nx_huge_pages=force nosmt=force l1d_flush=on spec_rstack_overflow=safe-ret gather_data_sampling=force reg_file_data_sampling=on random.trust_bootloader=off random.trust_cpu=off intel_iommu=on amd_iommu=force_isolation efi=disable_early_pci_dma iommu=force iommu.passthrough=0 iommu.strict=1 slab_nomerge init_on_alloc=1 init_on_free=1 pti=on vsyscall=none ia32_emulation=0 page_alloc.shuffle=1 randomize_kstack_offset=on debugfs=off $(cat /etc/kernel/cmdline)" > /etc/kernel/cmdline
@@ -82,6 +82,9 @@ update-initramfs -u
 curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/security/limits.d/30-disable-coredump.conf | tee /etc/security/limits.d/30-disable-coredump.conf > /dev/null
 mkdir -p /etc/systemd/coredump.conf.d
 curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/systemd/coredump.conf.d/disable.conf | tee /etc/systemd/coredump.conf.d/disable.conf > /dev/null
+
+# Setup ZRAM
+curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/etc/systemd/zram-generator.conf | tee /etc/systemd/zram-generator.conf > /dev/null
 
 # Disable Nagging
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
