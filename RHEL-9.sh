f43fd0430afb92daaa7411151f5dcdf20e7ec6ec
Tommy <contact@tommytran.io>
2025-05-29 16:17:17 -0700

    Update license year
    Signed-off-by: Tommy <contact@tommytran.io>

--- RHEL-9.sh
+++ RHEL-9.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2021-2024 Thien Tran
+# Copyright (C) 2021-2025 Thien Tran
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
