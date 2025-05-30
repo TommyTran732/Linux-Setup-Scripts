09fa8f2262dc65c00ced78da99556dd5ce535c8e
Tommy <contact@tommytran.io>
2025-05-30 15:58:38 -0700

    Configure Proxmox automatic upgrades
    Signed-off-by: Tommy <contact@tommytran.io>

--- Proxmox-8.sh
+++ Proxmox-8.sh
@@ -69,7 +69,7 @@ else
 fi
 
 # Install packages
-apt install -y "${microcode}" proxmox-kernel-6.14 systemd-zram-generator tuned
+apt install -y "${microcode}" proxmox-kernel-6.14 unattended-upgrades systemd-zram-generator tuned
 
 ### This part assumes that you are using systemd-boot
 echo "mitigations=auto,nosmt spectre_v2=on spectre_bhi=on spec_store_bypass_disable=on tsx=off kvm.nx_huge_pages=force nosmt=force l1d_flush=on l1tf=full,force kvm-intel.vmentry_l1d_flush=always spec_rstack_overflow=safe-ret gather_data_sampling=force reg_file_data_sampling=on random.trust_bootloader=off random.trust_cpu=off intel_iommu=on amd_iommu=force_isolation efi=disable_early_pci_dma iommu=force iommu.passthrough=0 iommu.strict=1 slab_nomerge init_on_alloc=1 init_on_free=1 pti=on vsyscall=none ia32_emulation=0 page_alloc.shuffle=1 randomize_kstack_offset=on debugfs=off lockdown=confidentiality module.sig_enforce=1 nomodeset $(cat /etc/kernel/cmdline)" > /etc/kernel/cmdline
@@ -96,6 +96,9 @@ curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/main/
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 
 systemctl restart pveproxy.service
+
+# Configure automatic updates
+curl -s https://raw.githubusercontent.com/TommyTran732/Linux-Setup-Scripts/refs/heads/main/etc/apt/apt.conf.d/52unattended-upgrades-local-proxmox | tee /etc/apt/apt.conf.d/52unattended-upgrades-local > /dev/null
 
 # Setup tuned
 tuned-adm profile virtual-host
